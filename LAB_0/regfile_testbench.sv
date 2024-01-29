dcecf2206152ab7f64a5753c17142c9eb9318270
jordanpaul98 <147276895+jordanpaul98@users.noreply.github.com>
2024-01-29 16:36:58 -0600

    Update regfile_testbench.sv

--- LAB_0/regfile_testbench.sv
+++ LAB_0/regfile_testbench.sv
@@ -1,5 +1,7 @@
 // ECEN 4243 Lab 0 regfile testbench
 
+
+
 module stimulus();
 
    logic  clock;
@@ -11,10 +13,12 @@ module stimulus();
    logic [31:0] rd1;
    logic [31:0] rd2;
 	
-   logic [5:0] count, errors;
+   logic [31:0] count, errors;
    logic [31:0] rd1_expect, rd2_expect;
    
    logic cycle2, break_test;
+
+   logic [111:0] testvectors[10000:0];
    
    
    
@@ -32,7 +36,8 @@ module stimulus();
      end
 
    initial
-     begin
+     begin	
+		$readmemb("regfile.tv", testvectors);
 		count = 0; errors = 0;
 		we3 = 1'b1; // set write enable high
 		
@@ -44,43 +49,13 @@ always @(posedge clock)
 	begin
 		// Only need to add new case, will default break the test
 		// once all the cases seqeuntly tested
-		case (count) 
-			0: 		 begin
-					// read addr 1, read addr2, write addr, write data
-					// read data 1 expected, read data 2 expected
-				#1; {ra1, ra2, wa3, wd3} = {5'd0, 5'd0, 5'd0, 32'h00000000};
-					{rd1_expect, rd2_expect} = {32'h00000000, 32'h00000000}; end
-					
-			1: 		 begin
-				#1; {ra1, ra2, wa3, wd3} = {5'd0, 5'd0, 5'd0, 32'hffffffff};
-					{rd1_expect, rd2_expect} = {32'h00000000, 32'h00000000}; end
-			
-			2: 		 begin
-				#1; {ra1, ra2, wa3, wd3} = {5'd1, 5'd0, 5'd1, 32'hffffffff};
-					{rd1_expect, rd2_expect} = {32'hffffffff, 32'h00000000}; end
-			
-			3: 		 begin
-				#1; {ra1, ra2, wa3, wd3} = {5'd0, 5'd1, 5'd1, 32'hffffffff};
-					{rd1_expect, rd2_expect} = {32'h00000000, 32'hffffffff}; end
-					
-			4: 		 begin
-				#1; {ra1, ra2, wa3, wd3} = {5'd1, 5'd1, 5'd1, 32'hffffffff};
-					{rd1_expect, rd2_expect} = {32'hffffffff, 32'hffffffff}; end
-					
-			5: 		 begin
-				#1; {ra1, ra2, wa3, wd3} = {5'd1, 5'd1, 5'd1, 32'haaaaaaaa};
-					{rd1_expect, rd2_expect} = {32'haaaaaaaa, 32'haaaaaaaa}; end
-					
-			default: begin break_test = 1; end // raise the break flag if there are no more cases
-		endcase
+
+		#1; {ra1, ra2, wa3, wd3, rd1_expect, rd2_expect} = testvectors[count];
+		
 	end
 
 always @(negedge clock)
 	begin
-		if (break_test) begin // check see if break flag was raised. if so stop the test
-			$display("Test Complete: %d errors found", errors);
-			$stop; // stop the testbench
-		end
 		if (cycle2) begin // capture only after two clock cycles
 			if (rd1 !== rd1_expect | rd2 !== rd2_expect) begin
 				// check if read data 1 matches that of read data 1 expected: same for 2
@@ -88,9 +63,15 @@ always @(negedge clock)
 				errors = errors + 1;
 			end
 			count = count + 1;
+			
+			if (testvectors[count] === 'bx) begin
+				$display("%d tests completed with %d errors", count, errors);
+				$stop;
+			end
 		end
 		cycle2 = ~cycle2; // toggle the cycle
 	end
 		
 
 endmodule // regfile_tb
+
